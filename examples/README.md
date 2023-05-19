8e539eb51a560cfa67ee9e7fdb14eadf4cbef807
cmsxbc <i@cmsis.me>
2023-05-19 23:48:23 +0800

    docs: add more examples

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,6 @@
 # calendarimg.sh Examples
 
-## default
+## Default
 
 ```bash
 for i in {0..362};do
@@ -10,7 +10,7 @@ done
 
 ![default](default.png)
 
-## modify_cell_width
+## Modify cell width
 
 ```bash
 CALENDARIMG_CELL_WIDTH=40
@@ -20,3 +20,142 @@ done
 ```
 
 ![modify_cell_width](modify_cell_width.png)
+
+## Modify border
+
+```bash
+CALENDARIMG_BORDER=1
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_border](modify_border.png)
+
+## Modify border style
+
+```bash
+CALENDARIMG_BORDER_STYLE="dashed"
+CALENDARIMG_NODATA_BORDER_STYLE="solid"
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_border_style](modify_border_style.png)
+
+## Modify padding
+
+```bash
+CALENDARIMG_PADDING=10
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_padding](modify_padding.png)
+
+## Modify margin and background color
+
+```bash
+CALENDARIMG_MARGIN=50
+CALENDARIMG_COLOR_BG="0 0 192"
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_margin_and_background_color](modify_margin_and_background_color.png)
+
+## Modify major
+
+```bash
+CALENDARIMG_MAJOR="col"
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_major](modify_major.png)
+
+## Modify rows
+
+```bash
+CALENDARIMG_ROWS=15
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_rows](modify_rows.png)
+
+## Modify cols
+
+```bash
+CALENDARIMG_COLS=30
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 ));
+done
+```
+
+![modify_cols](modify_cols.png)
+
+## Modify level colors
+
+```bash
+CALENDARIMG_LEVEL_LIMITS[0]=2
+CALENDARIMG_LEVEL_LIMITS[1]=3
+CALENDARIMG_LEVEL_LIMITS[2]=4
+CALENDARIMG_LEVEL_LIMITS[3]=5
+CALENDARIMG_LEVEL_LIMITS[4]=6
+CALENDARIMG_LEVEL_LIMITS[5]=7
+
+CALENDARIMG_LEVEL_COLORS[0]="255 0 0"
+CALENDARIMG_LEVEL_COLORS[1]="255 165 0"
+CALENDARIMG_LEVEL_COLORS[2]="255 255 0"
+CALENDARIMG_LEVEL_COLORS[3]="0 128 0"
+CALENDARIMG_LEVEL_COLORS[4]="0 0 255"
+CALENDARIMG_LEVEL_COLORS[5]="75 0 130"
+CALENDARIMG_LEVEL_COLORS[6]="238 130 238"
+
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7 + 1));
+done
+```
+
+![modify_level_colors](modify_level_colors.png)
+
+## Show summary number
+
+```bash
+CALENDARIMG_SUMMARY_NUMBER=enabled
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7));
+done
+```
+
+![show_summary_number](show_summary_number.png)
+
+## Reversed data
+
+```bash
+CALENDARIMG_DATA_ORDER=reversed
+for i in {0..362};do
+    CALENDARIMG_DATA[i]=$(( i % 7));
+done
+```
+
+![reversed_data](reversed_data.png)
+
+## Lack some data
+
+```bash
+for i in {0..363};do
+    if [[ $(( i % 13 )) -eq 3 ]];then
+        continue
+    fi
+    CALENDARIMG_DATA[i]=$(( i % 7));
+done
+```
+
+![lack_some_data](lack_some_data.png)
--- gen_examples.sh
+++ gen_examples.sh
@@ -25,44 +25,139 @@ function get_script {
 source calendarimg.sh
 
 function gen {
-    local name
+    local name name4func
     name="${1}.ppm"
+    name4func="config_$1"
+    calendarimg_reset_default
+    eval "$name4func"
     calendarimg_generate "$name"
     topng "$name"
 }
 
-function default {
+function config_default {
     for i in {0..362};do
         CALENDARIMG_DATA[i]=$(( i % 7 ));
     done
 }
 
-function modify_cell_width {
+function config_modify_cell_width {
     CALENDARIMG_CELL_WIDTH=40
     for i in {0..362};do
         CALENDARIMG_DATA[i]=$(( i % 7 ));
     done
 }
 
+function config_modify_border {
+    CALENDARIMG_BORDER=1
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_border_style {
+    CALENDARIMG_BORDER_STYLE="dashed"
+    CALENDARIMG_NODATA_BORDER_STYLE="solid"
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_padding {
+    CALENDARIMG_PADDING=10
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_margin_and_background_color {
+    CALENDARIMG_MARGIN=50
+    CALENDARIMG_COLOR_BG="0 0 192"
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_major {
+    CALENDARIMG_MAJOR="col"
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_rows {
+    CALENDARIMG_ROWS=15
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_cols {
+    CALENDARIMG_COLS=30
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 ));
+    done
+}
+
+function config_modify_level_colors {
+    CALENDARIMG_LEVEL_LIMITS[0]=2
+    CALENDARIMG_LEVEL_LIMITS[1]=3
+    CALENDARIMG_LEVEL_LIMITS[2]=4
+    CALENDARIMG_LEVEL_LIMITS[3]=5
+    CALENDARIMG_LEVEL_LIMITS[4]=6
+    CALENDARIMG_LEVEL_LIMITS[5]=7
+
+    CALENDARIMG_LEVEL_COLORS[0]="255 0 0"
+    CALENDARIMG_LEVEL_COLORS[1]="255 165 0"
+    CALENDARIMG_LEVEL_COLORS[2]="255 255 0"
+    CALENDARIMG_LEVEL_COLORS[3]="0 128 0"
+    CALENDARIMG_LEVEL_COLORS[4]="0 0 255"
+    CALENDARIMG_LEVEL_COLORS[5]="75 0 130"
+    CALENDARIMG_LEVEL_COLORS[6]="238 130 238"
+
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7 + 1));
+    done
+}
+
+function config_show_summary_number {
+    CALENDARIMG_SUMMARY_NUMBER=enabled
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7));
+    done
+}
+
+function config_reversed_data {
+    CALENDARIMG_DATA_ORDER=reversed
+    for i in {0..362};do
+        CALENDARIMG_DATA[i]=$(( i % 7));
+    done
+}
+
 function gen_readme_item {
-    local name
+    local name name4human name4func
     name="$1"
-    calendarimg_reset_default
-    eval "$name"
-    gen "$name"
-    echo -e "\n## ${name}\n"
+    name4human="${name//_/ }"
+    name4func="config_$name"
+    echo -e "\n## ${name4human^}\n"
     echo '```bash'
-    get_script "$name"
+    get_script "$name4func"
     echo '```'
     echo -e "\n![${name}](${name}.png)"
 }
 
 function gen_readme {
     echo "# calendarimg.sh Examples" > "$README_PATH"
-    gen_readme_item "default" >> "$README_PATH"
-    gen_readme_item "modify_cell_width" >> "$README_PATH"
+    grep "^function config_" "$SELF_PATH" | awk '{print $2}' | cut -c8- | while read -r item; do
+        echo "deal $item ..."
+        gen "$item"
+        gen_readme_item "$item" >> "$README_PATH"
+    done
 }
 
-rm -r examples
-mkdir examples
-gen_readme
+if [[ -n "$1" ]];then
+    gen "$1"
+else
+    rm -r examples
+    mkdir examples
+    gen_readme
+fi
